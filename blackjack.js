ae22bba695e8e4b18d68b389bc1e324d29db5d68
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2020-09-13 14:11:55 -0400

    basic styling

---
+++ blackjack.js
@@ -0,0 +1,14 @@
+
+// Space to put visible hands 
+const dealerHands = document.querySelector('#dealer-hand')
+const dealerHandSpace = document.createElement('div')
+dealerHands.appendChild(dealerHandSpace)
+dealerHandSpace.style.backgroundColor = 'white';
+dealerHandSpace.style.height ='70px'
+
+const playerHands = document.querySelector('#player-hand')
+const playerHandSpace = document.createElement('div')
+playerHands.appendChild(playerHandSpace)
+playerHandSpace.style.backgroundColor = 'white';
+playerHandSpace.style.height ='70px'
+
